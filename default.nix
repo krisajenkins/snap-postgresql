324af65a0455905dda2a9a63928702fe0d20db3f
Kris Jenkins <krisajenkins@gmail.com>
2015-05-12 17:59:12 +0100

    Updating for Snap 0.14.

--- default.nix
+++ default.nix
@@ -4,8 +4,8 @@ cabal.mkDerivation (self: {
   pname = "snap-postgresql";
   version = "0.1.0.0";
   src = ./.;
-  isLibrary = false;
-  isExecutable = true;
+  isLibrary = true;
+  isExecutable = false;
   buildTools = [ cabalInstall ];
   buildDepends = [esqueleto persistent persistentTemplate persistentPostgresql snap snapCore];
   meta = {
--- src/Snap/Snaplet/Postgresql.hs
+++ src/Snap/Snaplet/Postgresql.hs
@@ -1,21 +1,23 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Snap.Snaplet.Postgresql where
 
+import           Control.Monad.IO.Class
 import           Control.Monad.Logger
+import           Control.Monad.State.Class
 import           Data.ByteString
-import           Data.Maybe
-import           Database.Esqueleto
+import           Database.Esqueleto          hiding (get)
 import           Database.Persist            ()
 import           Database.Persist.Postgresql (createPostgresqlPool)
 import           Snap
 
 initDb :: ByteString -> Int -> SnapletInit a ConnectionPool
 initDb dbConnectString poolSize =
-  makeSnaplet "connection-pool" "A simple connection pool" Nothing .
+  makeSnaplet "connection-pool" "A simple Postgresql connection pool" Nothing .
   liftIO . runNoLoggingT $
   createPostgresqlPool dbConnectString poolSize
 
 queryHandler :: SqlPersistM b -> Handler a ConnectionPool b
 queryHandler query =
-  do conn <- Snap.get
-     liftIO (runSqlPersistMPool query conn)
+  do conn <- get
+     liftIO $
+       runSqlPersistMPool query conn
